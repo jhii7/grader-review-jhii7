f07a6e8711addaa840adecdfeff13f2b14cd83aa
jhii7 <156358567+jhii7@users.noreply.github.com>
2024-02-15 11:36:28 -0800

    added progress bar

--- grade.sh
+++ grade.sh
@@ -5,11 +5,13 @@ rm -rf grading-area
 
 mkdir grading-area
 
+echo "Loading: [     ]"
 git clone --quiet $1 student-submission
+echo -e "\e[1ALoading: [#    ]"
 
 if ! [ -f student-submission/ListExamples.java ]
 then
-    echo "Wrong file submitted"
+    echo "Wrong file submitted, check file name!"
     exit 1
 fi
 
@@ -17,21 +19,24 @@ cp TestListExamples.java student-submission/ListExamples.java grading-area
 cp -r lib grading-area
 
 cd grading-area
-
+echo -e "\e[1ALoading: [##   ]"
 javac -cp $CPATH *.java
 if [ $? -ne 0 ]
 then
-    echo "Compilation error"
+    echo "Compilation error, check your semicolons or brackets!"
     exit 1
 fi
 
+echo -e "\e[1ALoading: [###  ]"
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > grade-output.txt
 
+echo -e "\e[1ALoading: [#### ]"
 DIDFAIL=$(grep -c "FAILURES" grade-output.txt)
+echo -e "\e[1ALoading: [#####] Done!"
 if [ $DIDFAIL > 0 ]
 then
-    echo "There was a failed test."
+    echo -e "\e[1AThere was a failed test."
     exit 1
 fi
-echo "All tests successful! :)"
+echo -e "\e[1AAll tests successful! :)"
 
