1e70af4e26b96b7dd0a0688f51c6d02e2e1b1520
jhii7 <156358567+jhii7@users.noreply.github.com>
2024-02-15 11:04:19 -0800

    grade.sh change

--- grade.sh
+++ grade.sh
@@ -1,16 +1,37 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone --quiet $1 student-submission
 
+if ! [ -f student-submission/ListExamples.java ]
+then
+    echo "Wrong file submitted"
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]
+then
+    echo "Compilation error"
+    exit 1
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > grade-output.txt
+
+DIDFAIL=$(grep -c "FAILURES" grade-output.txt)
+if [ $DIDFAIL > 0 ]
+then
+    echo "There was a failed test."
+    exit 1
+fi
+echo "All tests successful! :)"
+
